696e7d2cc2f975eb8bef813a1b78cc6716f013a0
Estio Nurcahyanto <esttio2@gmail.com>
2019-12-24 23:41:39 +0700

    Updates

---
+++ precache-manifest.9c37c0aa45ff9c6adc5c900406f59684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989add164e7ccad87ac07919fd9edee7",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f4603ce1801f56c4563a",
+    "url": "/my-app/static/css/main.77651695.chunk.css"
+  },
+  {
+    "revision": "60fb47900b44368120d7",
+    "url": "/my-app/static/js/2.7bce059f.chunk.js"
+  },
+  {
+    "revision": "8e58f4f79fb5d84210c8b6d01742c8e6",
+    "url": "/my-app/static/js/2.7bce059f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4603ce1801f56c4563a",
+    "url": "/my-app/static/js/main.5b09d503.chunk.js"
+  },
+  {
+    "revision": "f7ac5d5c60d92816e82b",
+    "url": "/my-app/static/js/runtime-main.37949d88.js"
+  }
+]);
